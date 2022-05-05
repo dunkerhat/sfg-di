1a124f71ac44b74cfb88d96a13e00ce098df37ef
mcappello <mattia.cappello@objectway.com>
2022-05-05 23:56:07 +0200

    refactor

--- src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,36 @@
 package com.example.sfgdi.config;
 
+import com.example.sfgdi.service.I18nEnglishServiceImpl;
+import com.example.sfgdi.service.I18nSpanishServiceImpl;
 import com.example.sfgdi.service.greetings.ConstructorGreetingServiceImpl;
+import com.example.sfgdi.service.greetings.PrimaryGreetingService;
 import com.example.sfgdi.service.greetings.PropertyGreetingServicImpl;
 import com.example.sfgdi.service.greetings.SetterGreetingServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
-//@Configuration
+@Configuration
 public class GreetingServiceConfig {
+
+    @Bean("i18nService")
+    @Profile({"ES", "default"})
+    public I18nSpanishServiceImpl I18nSpanishServiceImpl(){
+        return new I18nSpanishServiceImpl();
+
+    } @Bean
+    @Profile("EN")
+    public I18nEnglishServiceImpl i18nService(){
+        return new I18nEnglishServiceImpl();
+    }
+
+    @Bean
+    @Primary
+    public PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     public ConstructorGreetingServiceImpl constructorGreetingServiceImpl(){
--- src/main/java/com/example/sfgdi/service/I18nEnglishServiceImpl.java
+++ src/main/java/com/example/sfgdi/service/I18nEnglishServiceImpl.java
@@ -1,11 +1,7 @@
 package com.example.sfgdi.service;
 
 import com.example.sfgdi.service.greetings.GreetingService;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Profile("EN")
-@Service("i18nService")
 public class I18nEnglishServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
